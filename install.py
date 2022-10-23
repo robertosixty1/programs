8a479c3b97e40ca8d6dfd8d2d99e883871aef28d
robertosixty1 <roberto011herminegildo@gmail.com>
2022-10-23 12:08:08 -0300

    Update installations script to install nerd fonts

--- install.py
+++ install.py
@@ -1,6 +1,8 @@
 #!/bin/env python3
 
-from os import getenv
+from os import getenv, listdir, makedirs, walk
+from os.path import isdir, isfile, join, dirname, basename
+from shutil import copy, move
 from subprocess import run
 
 def get_programs_from_packagestxt(f):
@@ -10,4 +12,27 @@ if getenv("USER") != "root":
     run(["sudo", "python", __file__])
     exit(0)
 
+HOME = "/home/" + listdir("/home")[0]
+
 run(["apt", "install"] + get_programs_from_packagestxt("packages.ubuntu.txt"))
+
+# install fonts
+
+if not isfile(HOME + "/.cache/fonts.zip"):
+    run(["wget", 
+        "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/FiraMono.zip",
+        "-O",
+        HOME + "/.cache/fonts.zip"])
+    if not isdir(HOME + "/.cache/fonts"):
+        makedirs(HOME + "/.cache/fonts")
+    run(["unzip", HOME + "/.cache/fonts.zip", "-d", HOME + "/.cache/fonts"])
+
+    for directory, folders, files in walk(HOME + "/.cache/fonts"):
+        for f in files:
+            fil = join(directory, f)
+            move(fil, join(dirname(fil), basename(fil).replace(' ', '')))
+
+    fonts = listdir(HOME + "/.cache/fonts")
+    for f in fonts:
+        if not (f.endswith(".md") or f.endswith(".txt") or f.endswith(".MD") or f.endswith(".TXT")):
+            copy(HOME + "/.cache/fonts/" + f, "/usr/share/fonts/" + f)
