a648934fcdec261bddf71170b64227f010749f59
robertosixty1 <roberto011herminegildo@gmail.com>
2022-12-10 00:43:48 +0000

    Fix `install-wm.py`

--- install-wm.py
+++ install-wm.py
@@ -1,4 +1,8 @@
+#!/bin/env python3
+
 from subprocess import run
+from os import getenv
+from sys import argv
 
 if getenv("USER") != "root":
     run(["sudo", "python", __file__] + argv[1:])
@@ -7,4 +11,4 @@ if getenv("USER") != "root":
 def get_programs_from_packagestxt(f):
     return open(f).read().split()
 
-run(["pacman", "-S", "--needed", get_programs_from_packagestxt("packages.archwm.txt")])
+run(["pacman", "-S", "--needed"] + get_programs_from_packagestxt("packages.archwm.txt"))
